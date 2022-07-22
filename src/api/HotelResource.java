fdbd8ffa972dfa1afd78f0c9765dbca0eba42755
mistermcghee27 <77115137+mistermcghee27@users.noreply.github.com>
2022-07-21 20:30:42 -0400

    finished Hotel Resource

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -1,19 +1,49 @@
 package api;
 
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
 import service.CustomerService;
 
-public class HotelResource {
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
 
+public class HotelResource {
     private static HotelResource single_instance = null;
 
-    private HotelResource() {
-    }
+    private HotelResource(){}
 
-    public static HotelResource getInstance() {
+    public static HotelResource getInstance(){
         if (single_instance == null) {
             single_instance = new HotelResource();
         }
         return getInstance();
+    }
+
+    public Customer getCustomer(String email){
+
+        return getCustomer(email);
+    }
+
+    public void createACustomer(String email, String firstName, String lastName){
 
     }
+
+    public IRoom getRoom(String roomNumber){
+        return single_instance.getRoom(roomNumber);
+    }
+
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date CheckOutDate){
+        return new Reservation();
+    }
+
+    public Collection<Reservation> getCustomersReservations(String customerEmail){
+        return new HashSet<>();
+    }
+
+    public Collection<IRoom> findARoom(Date checkIn, Date checkOut){
+        return new HashSet<>();
+    }
+
 }
